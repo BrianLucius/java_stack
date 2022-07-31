62b82c965154f361baa0af105774e62d4e21fde1
Brian Lucius <brian@brianlucius.net>
2022-07-31 16:09:18 -0600

    Add first Java programs

---
+++ fundamentals/first_java_program/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is LJ");
+        System.out.println("I am today years old");
+        System.out.println("My hometown is Denver, CO");
+    }
+}
