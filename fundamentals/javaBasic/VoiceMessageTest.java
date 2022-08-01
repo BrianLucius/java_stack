401646cbf577ebb7fcafcb0365873acfb76a6983
Brian Lucius <brian@brianlucius.net>
2022-08-01 15:14:29 -0600

    Add W1D1 afternoon lecture

--- fundamentals/javaBasic/VoiceMessageTest.java
+++ fundamentals/javaBasic/VoiceMessageTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class VoiceMessageTest{
     public static void main(String[] args) {
         VoiceMessageUtil myMachine = new VoiceMessageUtil();
@@ -9,5 +11,48 @@ class VoiceMessageTest{
         System.out.println(myMachine.greeting(10));
 
         System.out.println(myMachine.getCurrentDate());
+
+        int[] messagesPerDay1 = new int[5];
+        messagesPerDay1[0] = 5;
+        messagesPerDay1[1] = 8;
+        messagesPerDay1[2] = 6;
+
+        for(int i=0; i<messagesPerDay1.length; i++) {
+            System.out.println(messagesPerDay1[i]);
+        }
+
+        int[] messagesPerDay2 = {5, 8 , 6};
+
+        for(int i=0; i<messagesPerDay2.length; i++) {
+            System.out.println("Length: "+ messagesPerDay2.length);
+            System.out.println(messagesPerDay1[i]);
+        }
+
+        String[] messages = {"please call back!", "Make sure you install JDK"};
+        for(int i=0; i<messages.length; i++) {
+            System.out.println("Length: "+ messages.length);
+            System.out.println(messages[i]);
+        }
+
+        System.out.println("Total messages: " + myMachine.totalMessages(messagesPerDay1));
+
+        myMachine.printMessages(messages);
+
+        ArrayList<String> messageArrayList = new ArrayList<String>();
+        messageArrayList.add("Finish Alfred bot");
+        messageArrayList.add("Finish Cafe Business Logic");
+        messageArrayList.add("Read ahead for OOP");
+
+        myMachine.printMessages(messageArrayList);
+
+        int days = 7;
+        double cost = 10.99; // days + cost
+
+        double totalCost = days * cost;
+        System.out.println("Total Cost: "+totalCost);
+
+        int roundedTotalCost = days * (int)cost;
+        // int roundedTotalCost = (int) (days * cost);
+        System.out.println("Rounded total cost: " + roundedTotalCost);
     }
 }
--- fundamentals/javaBasic/VoiceMessageUtil.java
+++ fundamentals/javaBasic/VoiceMessageUtil.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 class VoiceMessageUtil{
     public String greeting() {
@@ -16,5 +17,37 @@ class VoiceMessageUtil{
     public String getCurrentDate() {
         Date today = new Date();
         return "Today is "+today;
+    }
+
+    public int totalMessages(int[] numArr){
+        int sum = 0;
+        for(int i=0; i < numArr.length; i++) {
+            sum+=numArr[i];
+        }
+        return sum;
+    }
+
+    public void printMessages(String[] msgArr){
+        //standard for loop
+        // for (int i=0; i<msgArr.length; i++) {
+        //     System.out.println("Messages: " + msgArr[i]);
+        // }
+
+        //enhanced for loop
+        for (String message: msgArr) {
+            System.out.println("Enhanced Messages: " + message);
+        }
+    }
+
+    public void printMessages(ArrayList<String> msgArr){
+        //standard for loop
+        // for (int i=0; i<msgArr.size(); i++) {
+        //     System.out.println("Messages array list: " + msgArr.get(i));
+        // }
+
+        //enhanced for loop
+        for (String message: msgArr) {
+            System.out.println("Enhanced Messages array list: " + message);
+        }
     }
 }
