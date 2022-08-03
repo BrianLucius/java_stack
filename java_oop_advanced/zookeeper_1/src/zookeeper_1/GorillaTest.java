0c32fffd89e94af72cc023c13a6342bed94d5e34
Brian Lucius <brian@brianlucius.net>
2022-08-02 21:42:43 -0600

    Add Zookeeper 1 exercise

---
+++ java_oop_advanced/zookeeper_1/src/zookeeper_1/GorillaTest.java
@@ -0,0 +1,29 @@
+package zookeeper_1;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla george = new Gorilla();
+		george.displayEnergy();
+		
+		george.throwSomething("a turd");
+		george.displayEnergy();
+		
+		george.eatBananas();
+		george.displayEnergy();
+		
+		george.throwSomething("a pink flamingo");
+		george.displayEnergy();
+		
+		george.eatBananas();
+		george.displayEnergy();
+		
+		george.throwSomething("a bunch of bananas");
+		george.displayEnergy();
+		
+		george.climb();
+		george.displayEnergy();
+
+	}
+
+}
---
+++ java_oop_advanced/zookeeper_1/src/zookeeper_1/Mammal.java
@@ -0,0 +1,24 @@
+package zookeeper_1;
+
+public class Mammal {
+	private int energyLevel;
+	
+	public Mammal() { 
+		energyLevel = 100;
+	}
+	
+	public int displayEnergy() {
+		System.out.printf("Energy level: %s\n", energyLevel);
+		return energyLevel;
+	}
+
+	public int getEnergyLevel() {
+		return energyLevel;
+	}
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+	
+	
+}
