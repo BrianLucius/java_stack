233dafc32148d9ad0fab6913951397dbd8f2083d
Brian Lucius <brian@brianlucius.net>
2022-08-15 12:10:12 -0600

    Add missing annotation

--- javaSpring/logreg-demo/src/main/java/net/brianlucius/logregdemo/repositories/UserRepo.java
+++ javaSpring/logreg-demo/src/main/java/net/brianlucius/logregdemo/repositories/UserRepo.java
@@ -3,9 +3,11 @@ package net.brianlucius.logregdemo.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import net.brianlucius.logregdemo.models.User;
 
+@Repository
 public interface UserRepo extends CrudRepository<User, Long> {
 
 	//search for the email/account
